b0945f4aa5912d5502f963951fa69aa581d31916
Kaio Fonseca <mkaiofonseca@gmail.com>
2022-09-15 11:52:38 -0300

    configurações iniciais

---
+++ src/pages/DashBoard/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DashBoard: React.FC = () => {
+    return (
+        <h1>DashBoard</h1>
+    );
+}
+
+export default DashBoard
+
+// fc é componente funcional, DAshBoarad é uma pagina de componente funcional
