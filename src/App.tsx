9ae58a39ed84dbcd18259ea7690dfb81d72bb43c
Kaio Fonseca <mkaiofonseca@gmail.com>
2022-09-18 18:43:43 -0300

    add styled-components

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import GlobalStyles from './styles/GlobalStyles'
+
+import DashBoard from './pages/DashBoard'
+
+const App: React.FC = () => {
+  return (
+    <>
+        <GlobalStyles />
+        <DashBoard />
+    </>
+  )
+}
+
+export default App
