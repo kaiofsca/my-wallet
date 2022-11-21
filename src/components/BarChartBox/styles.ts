846c477dd0f917da7d9321d9ea6aaad596ff1b9b
Kaio Fonseca <mkaiofonseca@gmail.com>
2022-11-21 11:54:00 -0300

    add animações e finalizando projeto

--- src/components/BarChartBox/styles.ts
+++ src/components/BarChartBox/styles.ts
@@ -1,8 +1,22 @@
-import styled from "styled-components";
+import styled, {keyframes} from "styled-components";
 
 interface ILegendProps {
     color: string;
 }
+
+const animate = keyframes`
+    0% {
+        transform: translateX(100px);
+        opacity: 0;
+    }
+    50% {
+        opacity: .3;
+    }
+    100% {
+        transform: translateX(0px);
+        opacity: 1;
+    }
+`
 
 export const Container = styled.div`
     width: 48%;
@@ -13,6 +27,8 @@ export const Container = styled.div`
     
     border-radius: 7px;
     display: flex;
+
+    animation: ${animate} .5s;
 
     @media(max-width: 1200px) {
         display: flex;
